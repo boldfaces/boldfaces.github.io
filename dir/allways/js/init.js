ae3e545012bad75667c28c31e2824f6c447b4743
boldfaces <ping@boldfac.es>
2022-07-15 14:49:55 +0800

    Minor fix on overflow

--- dir/allways/js/init.js
+++ dir/allways/js/init.js
@@ -21,7 +21,7 @@ function addCountries(whereto) {
 		   //navigationPosition: 'left',
 		   //navigationTooltips: ['Home', 'What we do', 'Services', 'More'],
 		   touchSensitivity: 8,
-		   normalScrollElements: '.x-overlist, .cs-options'
+		   normalScrollElements: '.x-overlist, .cs-options, .x-alert-body'
 	  });
 	}
 	addCountries('#countries');
